db601c10ec71d4cf9690fef4f26130886bc35e84
Boddapati <sb185408@ncr.com>
2020-05-12 05:55:56 +0800

    hello world changes

---
+++ src/Hello.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author sb185408
+ *
+ */
+public class Hello {
+
+	/**
+	 * 
+	 */
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World New");
+
+	}
+
+}
