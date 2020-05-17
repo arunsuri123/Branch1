260530e6e56a9b6caf19b9adc982e7242ddceef2
Boddapati <sb185408@ncr.com>
2020-05-18 06:58:50 +0800

    test commit

--- src/Hello.java
+++ src/Hello.java
@@ -20,7 +20,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World New");
+		System.out.println("Hello World Latest");
 
 	}
 
